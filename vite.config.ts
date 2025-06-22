b501634d5650d73f1f8fe08b1bf2d6c4a5b79a8f
aanujkhurana <aanujkhurana@gmail.com>
2025-06-22 19:19:34 +1000

    update package jsons and config files

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-// https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'index.ts'),
-      name: 'VuePortfolioChatbot',
-      fileName: (format) => `vue-portfolio-chatbot.${format}.js`,
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'ChatAssistant',
+      fileName: (format) => `chat-assistant.${format}.js`,
     },
     rollupOptions: {
       external: ['vue'],
